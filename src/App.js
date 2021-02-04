e7103c9714fe5549435738e94aca3907f34e1353
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-02-04 11:43:25 +0100

    event and input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
+import { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
-function App() {
-  return (
-    <div className="App">
-      <h1 className='my-h1'>Hello React !</h1>
-      <Person name="Ayoub" age='20' />
-      <Person name="Hamid" />
-      <Person name="Hafssa" />
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Hamid",
+        age: 20,
+      },
+    ],
+  };
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        {
+          name: "Hamada",
+          age: 75,
+        },
+      ],
+    });
+  };
+  switchNameHandler1 = () => {
+    this.setState({
+      persons: [
+        {
+          name: "hafsa",
+          age: 175,
+        },
+      ],
+    });
+  };
+  changeNameHandler = (event) => {
+    console.log(event);
+    this.setState({
+      persons: [
+        {
+          name: event.target.value,
+          age: 175,
+        },
+      ],
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1 className="my-h1">Hello React !</h1>
+        <button onClick={this.switchNameHandler1}>Clique Me !</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler}
+          changed={this.changeNameHandler}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,13 @@
 import React from "react";
 const Person = (props) => {
   return (
-    <p>
-      I am a {props.name} and I am {props.age}
-    </p>
+    <div>
+      <input type="text" onChange={props.changed} value={props.name} />
+      <p>
+        I am a {props.name} and I am {props.age}
+      </p>
+      <button onClick={props.click}>my button</button>
+    </div>
   );
 };
 export default Person;
