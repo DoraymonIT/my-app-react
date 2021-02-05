c6365d521a5b0b54f9594f66d1ef2fbb4a6bab11
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-02-05 13:15:37 +0100

    style componments package

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      {
-        name: "Hamid",
-        age: 20,
-      },
+      { id: 1, name: "Hamid", age: 20 },
+      { id: 2, name: "Hafssa", age: 25 },
+      { id: 3, name: "Ayoub", age: 21 },
     ],
   };
   switchNameHandler = () => {
@@ -41,17 +40,42 @@ class App extends Component {
       ],
     });
   };
+  togglePersonsDataHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
   render() {
     return (
       <div className="App">
         <h1 className="my-h1">Hello React !</h1>
-        <button onClick={this.switchNameHandler1}>Clique Me !</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-          click={this.switchNameHandler}
-          changed={this.changeNameHandler}
-        />
+        {this.state.persons.map((p, index) => {
+          return (
+            <Person
+              name={p.name}
+              age={p.age}
+              click={() => {
+                this.deletePersonHandler(index);
+              }}
+              key={p.id}
+            />
+          );
+        })}
+        {/* <button onClick={this.togglePersonsDataHandler}>Clique Me !</button> */}
+        {/* {this.state.showPersons === true ? (
+          <div>F
+            <Person
+              name={this.state.persons[0].name}
+              age={this.state.persons[0].age}
+              click={this.switchNameHandler}
+              changed={this.changeNameHandler}
+            />
+          </div>
+        ) : null} */}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,27 @@
 import React from "react";
+import "./Person.css";
+import logo from "./logo192.png";
+import styled from "styled-components";
+
 const Person = (props) => {
+  const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+  `;
   return (
     <div>
-      <input type="text" onChange={props.changed} value={props.name} />
+      {/* <input type="text" onChange={props.changed} value={props.name} /> */}
       <p>
         I am a {props.name} and I am {props.age}
+        <Button onClick={props.click}>Delete Me</Button>
       </p>
-      <button onClick={props.click}>my button</button>
+      <hr />
+      <img src={logo} alt="Logo" />
+      {/* <button onClick={props.click}>my button</button> */}
     </div>
   );
 };
